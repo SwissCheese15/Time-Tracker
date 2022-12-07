05159ec17c47d2a96d1b1e9d2341bd5d406e38f7
SwissCheese15 <110164826+SwissCheese15@users.noreply.github.com>
2022-12-07 10:57:57 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Redux, Redux Toolkit installed. Blank store and slice files ready.
+// Router installed. Routes set up in main index.js
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Manuel's Test Day at SmartFactory
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
